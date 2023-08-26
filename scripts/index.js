950a31d9806387a5269236e06a26e5489d312ccc
Leonardo Vargas <leu1607@hotmail.com>
2023-08-26 15:38:53 -0300

    integrando a lib AOS

--- scripts/index.js
+++ scripts/index.js
@@ -31,11 +31,17 @@ const createTags = (projectTags) => {
   return elemTags
 }
 
-const createProject = (project) => {
+const createProject = (project, index) => {
   const elemProject = document.createElement('a')
 
   elemProject.setAttribute('href', project.link)
   elemProject.setAttribute('target', '_blank')
+
+  elemProject.setAttribute('data-aos', 'zoom-in-up')
+  elemProject.setAttribute('data-aos-duration', '800')
+  elemProject.setAttribute('data-aos-easing', 'ease-in-out')
+  elemProject.setAttribute('data-aos-offset', '-100')
+  elemProject.setAttribute('data-aos-delay', 300 * (index + 1))
   
   elemProject.classList.add('project')
 
@@ -52,8 +58,8 @@ const createProject = (project) => {
 }
 
 const loadProjects = (projects) => {
-  projects.forEach(project => {
-    elemProjects.appendChild(createProject(project))
+  projects.forEach((project, index) => {
+    elemProjects.appendChild(createProject(project, index))
   });
 }
 
